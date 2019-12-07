ca481ea6b9a9e568515f68bdd3a1d736fcbf78ad
AbhishekSatyam96 <abhishek.satyam96@gmail.com>
2019-12-07 22:07:41 +0530

    Updates

---
+++ precache-manifest.2c94eb9b3f584a2f86def5144232ce56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "497c78df31463465306f1ceb5b05cd42",
+    "url": "/Github-Card-App/./index.html"
+  },
+  {
+    "revision": "63bc30cfd38fc5f5e009",
+    "url": "/Github-Card-App/./static/css/main.42c5626f.chunk.css"
+  },
+  {
+    "revision": "e71a68c9e11a8524356c",
+    "url": "/Github-Card-App/./static/js/2.c1a391c3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Github-Card-App/./static/js/2.c1a391c3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "63bc30cfd38fc5f5e009",
+    "url": "/Github-Card-App/./static/js/main.e9359ef0.chunk.js"
+  },
+  {
+    "revision": "6221a9a85fe537513b62",
+    "url": "/Github-Card-App/./static/js/runtime-main.15b08a8f.js"
+  }
+]);
